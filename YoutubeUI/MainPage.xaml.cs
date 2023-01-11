67255f689ad744317b799fe299afc2bbe9cf3ccb
kana2022 <kkanat94@gmail.com>
2023-01-11 16:27:59 +0600

    added some new icons

--- YoutubeUI/MainPage.xaml.cs
+++ YoutubeUI/MainPage.xaml.cs
@@ -6,5 +6,23 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private static IEnumerable<Tag> GetTags() =>
+        new List<Tag>()
+        {
+            new Tag("Все"),
+            new Tag("Видеоигры"),
+            new Tag("Дикая природа"),
+            new Tag("Сейчас в эфире"),
+            new Tag("Анимация"),
+            new Tag("Экшен и приключения"),
+            new Tag("Ремесла"),
+            new Tag("Последние опубликованные видео"),
+            new Tag("Просмотрено"),
+            new Tag("Новое для вас"),
+            new Tag("Отправить отзыв"),
+        };
 }
+
+public record struct Tag(string Name);
 
